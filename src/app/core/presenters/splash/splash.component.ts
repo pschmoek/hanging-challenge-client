42ced464f82f6a7aa325c16f76cfb1ab5340ddb8
Philip Schmoekel <philip.schmoekel@googlemail.com>
2017-12-09 19:22:50 +0100

    Refactoring login splash

--- src/app/core/presenters/splash/splash.component.ts
+++ src/app/core/presenters/splash/splash.component.ts
@@ -3,11 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-splash',
   template: `
-<h2>Es wird nach einer bestehenden Session gesucht...</h2>
-
-<app-facebook-login-button *ngIf="showFacebookLogin">
-</app-facebook-login-button>
-  `
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3">Hanging Challenge</h1>
+    <p class="lead">Please login.</p>
+    <hr class="my-4">
+    <app-facebook-login-button *ngIf="showFacebookLogin">
+    </app-facebook-login-button>
+  </div>
+</div>
+  `,
+  styles: [`
+  `]
 })
 export class SplashComponent {
 
